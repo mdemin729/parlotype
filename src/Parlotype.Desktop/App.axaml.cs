479e7500766083f119635ce763de8c5af6260cdc
msdemin <maximdemin1980@gmail.com>
2026-02-19 08:09:21 -0500

    feat: add ZLogger logging infrastructure
    - Add Microsoft.Extensions.Logging.Abstractions to Core
    - Add ZLogger v2.5.10 to Platform and Desktop
    - Configure colored console + daily rolling file logging in App.axaml.cs
    - Log files stored at AppData/Local/parlotype/logs/

--- src/Parlotype.Desktop/App.axaml.cs
+++ src/Parlotype.Desktop/App.axaml.cs
@@ -2,14 +2,20 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Parlotype.Desktop.ViewModels;
 using Parlotype.Desktop.Views;
 using Parlotype.Platform;
+using ZLogger;
 
 namespace Parlotype.Desktop;
 
 public class App : Application
 {
+    private static readonly string LogDirectory = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+        "parlotype", "logs");
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -18,6 +24,42 @@ public class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         var services = new ServiceCollection();
+
+        services.AddLogging(logging =>
+        {
+            logging.SetMinimumLevel(LogLevel.Debug);
+
+            logging.AddZLoggerConsole(options =>
+            {
+                options.UsePlainTextFormatter(formatter =>
+                {
+                    formatter.SetPrefixFormatter($"{0:yyyy-MM-dd HH:mm:ss.fff} [{1}] ",
+                        (in MessageTemplate template, in LogInfo info) =>
+                            template.Format(info.Timestamp, info.LogLevel));
+                    formatter.SetSuffixFormatter($" ({0})",
+                        (in MessageTemplate template, in LogInfo info) =>
+                            template.Format(info.Category));
+                });
+            });
+
+            logging.AddZLoggerRollingFile(options =>
+            {
+                options.UsePlainTextFormatter(formatter =>
+                {
+                    formatter.SetPrefixFormatter($"{0:yyyy-MM-dd HH:mm:ss.fff} [{1}] ",
+                        (in MessageTemplate template, in LogInfo info) =>
+                            template.Format(info.Timestamp, info.LogLevel));
+                    formatter.SetSuffixFormatter($" ({0})",
+                        (in MessageTemplate template, in LogInfo info) =>
+                            template.Format(info.Category));
+                });
+                options.FilePathSelector = (dt, seq) =>
+                    Path.Combine(LogDirectory, $"parlotype-{dt:yyyy-MM-dd}_{seq:000}.log");
+                options.RollingInterval = ZLogger.Providers.RollingInterval.Day;
+                options.RollingSizeKB = 10_240;
+            });
+        });
+
         services.AddPlatformServices();
         services.AddSingleton<SettingsViewModel>();
         services.AddSingleton<MainWindowViewModel>();
